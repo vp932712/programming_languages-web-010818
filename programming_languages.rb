8efbf758dca6a364ce3f8855ec2f639eaac4a1cb
Vaibhav Patel <vp932712@gmail.com>
2017-12-20 00:03:25 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  final = {}
+  languages.each do |key, value|
+
+   value.each do |key1, value1|
+
+       value1.each do |type, com|
+
+         final[key1] = {:type => com, :style => [key]}
+       end
+   end
+     end
+final[:javascript][:style].unshift(:oo)
+  final
+
 end
